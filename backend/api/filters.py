fdc461e79fa18b6999debca9dedc34a38516fb2b
Максим Титов <matitov2004@gmail.com>
2023-05-29 00:09:50 +0300

    api filters

---
+++ backend/api/filters.py
@@ -0,0 +1,46 @@
+from django_filters import rest_framework as filters
+from recipes.models import Ingredient, Recipe, Tag
+
+
+class RecipeFilter(filters.FilterSet):
+    tags = filters.ModelMultipleChoiceFilter(
+        field_name='tags__slug',
+        to_field_name='slug',
+        queryset=Tag.objects.all(),
+        label='Tags'
+    )
+    author = filters.CharFilter(
+        field_name='author__id',
+        lookup_expr='icontains'
+    )
+    is_favorited = filters.BooleanFilter(method='filter_is_favorited')
+    is_in_shopping_cart = filters.BooleanFilter(
+        method='filter_is_in_shopping_cart'
+    )
+
+    class Meta:
+        model = Recipe
+        fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
+
+    def filter_is_favorited(self, queryset, name, value):
+        if value:
+            return queryset.filter(favorite_recipe__user=self.request.user)
+        return queryset
+
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        if value:
+            return queryset.filter(
+                shopping_cart_recipe__user=self.request.user
+            )
+        return queryset
+
+
+class IngredientFilter(filters.FilterSet):
+    name = filters.CharFilter(
+        field_name='name',
+        lookup_expr='istartswith'
+    )
+
+    class Meta:
+        model = Ingredient
+        fields = ('name', 'measurement_unit')
